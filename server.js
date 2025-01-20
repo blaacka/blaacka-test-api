055a186b916eea99e1dd827f58b4fd591d26eb3e
Donatien SAMBIA <donatien.sambia@sogeti.com>
2025-01-20 12:01:54 +0100

    add package.json

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 2000;
 
 const countries = [
   { code: 'FR', name: 'France SIRLRIA NG' },
   { code: 'DE', name: 'Germany SIRLRIA NG' },
   { code: 'IT', name: 'Italy SIRLRIA NG' },
-  { code: 'ITxx', name: 'Norvege SIRLRIA NG xxxxxxx' },
+  { code: 'DD', name: 'Norvege SIRLRIA NG xxxxxxx' },
   // Ajoutez d'autres pays ici
 ];
 
@@ -14,15 +14,22 @@ const dangers = [
   { code: 'FR', name: 'DANGER AAAA' },
   { code: 'DE', name: 'DANGER BBBB' },
   { code: 'IT',  name: 'DANGER CCCC' },
-  { code: 'ITxx', name: 'DANGER DDDD' },
+  { code: 'DD', name: 'DANGER DDDD' },
   // Ajoutez d'autres pays ici
 ];
+
+const classeDangerRefListe = [{value:'AA',label:'DANGER AAAA'},{value:'BB',label:'DANGER BBBB'},{value:'CC',label:'DANGER CCCC'},{value:'DD',label:'DANGER DDDD'}];
+const classeDangerRefListe2 = [{value:'AA',label:'DANGER AAAA'},{value:'BB',label:'DANGER BBBB'},{value:'CC',label:'DANGER CCCC'},{value:'DD',label:'DANGER DDDD'},{value:'EE',label:'DANGER EEEEE'},{value:'FF',label:'DANGER FFFF'},,{value:'GG',label:'DANGER GGGG'}];
 
 app.get('/countries', (req, res) => {
   res.json(countries);
 });
 app.get('/dangers', (req, res) => {
   res.json(dangers);
+});
+
+app.get('/formsjs/dangers', (req, res) => {
+  res.json(classeDangerRefListe);
 });
 
 app.get('/', (req, res) => {
